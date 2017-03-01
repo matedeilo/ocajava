557e1f86ff66663110e69ff908c539497c5c0c97
Manuel Tejeda <mtejeda21@gmail.com>
2017-03-01 07:09:20 -0500

    first commit

---
+++ src/net/avantica/java/cert/chapter1basics/ExamQuestions.java
@@ -0,0 +1,10 @@
+
+public class ExamQuestions {
+
+    static int marks = 10;
+
+    public static void print() {
+        System.out.printf("100");
+    }
+
+}
---
+++ src/net/avantica/java/cert/chapter1basics/NoPackageDefined.java
@@ -0,0 +1,10 @@
+
+
+public class NoPackageDefined {
+
+    public String name;
+
+    private void print(){
+
+    }
+}
